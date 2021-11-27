afc68f817e7b5a1486db2235b8446f81f5cc2d0e
Mehtap Uğur <yaziliminval@hotmail.com>
2021-11-27 13:21:46 +0300

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,4 @@
+########### baştaki sudoku kodu
 import numpy as np
 import threading
 
@@ -64,3 +65,81 @@ t2 = threading.Thread(target=iki)
 
 t1.start()
 t2.start()
+
+############# nisa samurai okutup yazdırma
+import numpy as np 
+
+# samourai matrisi tanimlama
+samourai = np.zeros((21,21))
+
+#print(samourai)
+
+#txt dosyasini okuma modunda acma
+file = open("sudoku.txt","r")
+
+
+for i in range (21):
+    v=0
+    veri=file.readline()
+    for j in range (21):
+        if(i<6):                        #ilk 6 satiri txtden cekiyor
+            if(j>8 and j<12):
+                samourai[i][j]=0    #bosluk
+            else:
+                if(veri[v]=="*"):
+                    samourai[i][j]=0
+                    v=v+1
+                elif(veri[v]=="\n"):
+                    v=v+1
+                else:
+                    samourai[i][j]=int(veri[v])
+                    v=v+1
+        elif(i>5 and i<9):              # 7. 8. 9. satirlari cekiyor
+            if(veri[v]=='*'):
+                samourai[i][j]=0
+                v=v+1
+            elif(veri[v]=="\n"):
+                v=v
+            else:
+                samourai[i][j]=int(veri[v])
+                v=v+1
+        elif(i>8 and i<12):             #9 10 11. satirlar 
+            if(j<6 or j>14):
+                samourai[i][j]=0    #bosluk
+            elif(j>5 and j<15):
+                if(veri[v]=="*"):
+                    samourai[i][j]=0
+                    v=v+1
+                elif(veri[v]=="\n"):
+                    v=v+1
+                else:
+                    samourai[i][j]=int(veri[v])
+                    v=v+1
+        elif(i>11 and i<15):            #12 13 14. satirlar
+            if(veri[v]=='*'):
+                samourai[i][j]=0
+                v=v+1
+            elif(veri[v]=="\n"):
+                v=v
+            else:
+                samourai[i][j]=int(veri[v])
+                v=v+1          
+        elif(i>14):                     #son 6 satir 
+            if(j>8 and j<12):
+                samourai[i][j]=0    #bosluk
+            else:
+                if(veri[v]=="*"):
+                    samourai[i][j]=0
+                    v=v+1
+                elif(veri[v]=="\n"):
+                    v=v+1
+                else:
+                    samourai[i][j]=int(veri[v])
+                    v=v+1
+
+
+print(samourai)
+
+file.close()
+
+
